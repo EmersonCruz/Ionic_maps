7ade93c1abfab7c488df1e796ed6d9d5d7dd6f91
emersoncruz <emersoncruza@gmail.com>
2018-08-03 19:53:02 -0500

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { Platform } from 'ionic-angular';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -25,6 +28,8 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+    
+
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Platform } from 'ionic-angular';
 
 declare var google: any;
 
@@ -8,10 +10,30 @@ declare var google: any;
   templateUrl: 'home.html'
 })
 export class HomePage {
-  @ViewChild('map') mapRef: Element;
-  constructor(public navCtrl: NavController) {
+  
+  @ViewChild('map') mapRef: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController, private platform: Platform/*, private geolocation: Geolocation*/) {
+    /*platform.ready().then(() => {
+
+      // get current position
+     geolocation.getCurrentPosition().then(pos => {
+        console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+      });
+
+      const watch = geolocation.watchPosition().subscribe(pos => {
+        console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+      });
+
+      // to stop watching
+      watch.unsubscribe();
+
+    });*/
 
   }
+
+  
   ionViewDidLoad() {
   
     this.showMap();
